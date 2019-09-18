cc1d41285d6b899b214d2fe35aa0ffd523414aeb
Jeff Lucas <jslucas1@msn.com>
2019-09-18 08:29:33 -0500

    Add project files.

---
+++ Fall2019 PA 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fall2019_PA_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //PA2 Starter Code
+        }
+    }
+}
